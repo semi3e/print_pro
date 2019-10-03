ca664a3e32f213b1193ea7ef146fa31e046aa63c
ksshlee <ksshlee@naver.com>
2019-10-03 22:53:00 +0900

    Nav 바 제작

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* main page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+  res.render('home/main', { title: 'print_pro',message: '프젝 화이팅!' });
 });
+
+/* manual page. */
+router.get('/manual', function(req,res,next){
+  res.render('home/manual');
+});
+
+/*myongjin page. */
+router.get('/myongjin', function(req,res,next){
+  res.render('home/myongjin');
+});
+
+/*ogong page. */
+router.get('/ogong', function(req,res,netx){
+  res.render('home/ogong');
+});
+
+
 
 module.exports = router;
